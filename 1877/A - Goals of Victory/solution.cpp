03969cc4b06161d0144eac838d6d067051c13692
Vaibhavi <vaibhavi.s147@gmail.com>
2026-05-13 15:33:14 +0530

    Add Goals of Victory [A] from Codeforces

---
+++ 1877/A - Goals of Victory/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+          int n;
+          cin>>n;
+          int sum=0;
+          for(int i=0;i<n-1;i++){
+                int x;
+                cin>>x;
+                sum+=x;
+          }
+          cout<<0-sum<<endl;
+    }
+    
+ 
+    return 0;
+ 
+}
