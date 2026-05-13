c3dc4ab5d62c14fe537dee398099ee5ffcaab287
Vaibhavi <vaibhavi.s147@gmail.com>
2026-05-13 15:31:55 +0530

    Add Prefix Max [B] from Codeforces

---
+++ 2185/B - Prefix Max/solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> vec(n);
+        for(int i=0;i<n;i++) cin>>vec[i];
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            if(vec[i]>maxi){
+                maxi=vec[i];
+            }
+        }
+ 
+        cout<<maxi*n<<endl;  
+        
+ 
+        
+   
+ 
+        
+ 
+ 
+    }
+}
