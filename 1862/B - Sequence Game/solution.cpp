ea6dce1d8d23b609da16977b85839d78eaed40e9
Vaibhavi <vaibhavi.s147@gmail.com>
2026-05-13 15:33:35 +0530

    Add Sequence Game [B] from Codeforces

---
+++ 1862/B - Sequence Game/solution.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define ios_fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+ 
+// Data Types
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+ 
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+ 
+// Macros
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define REP(i, n) for (int i = 0; i < n; ++i)
+ 
+// Modular Arithmetic
+ll mod_add(ll a, ll b) { return (a + b) % MOD; }
+ll mod_sub(ll a, ll b) { return (a - b + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a * b) % MOD; }
+ 
+int main() {
+    ios_fast;
+    int t;
+    cin >> t;
+    while (t--) {
+        // Problem logic here
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+ 
+        for(int i=0;i<n-1;i++){
+            if(v[i]<=v[i+1]){
+                ans.push_back(v[i]);
+            }else{
+                ans.push_back(v[i]);
+                int val=v[i+1]-1;
+                if(val>0){
+                    ans.push_back(val);
+                }else{
+                    ans.push_back(1);
+                }
+                
+            }
+        }
+ 
+        ans.push_back(v[n-1]);
+    
+                
+        cout<<ans.size()<<endl;
+        for(auto el: ans){
+            cout<<el<<" ";
+        }
+        cout<<endl;    
+        
+    }
+    return 0;
+}
