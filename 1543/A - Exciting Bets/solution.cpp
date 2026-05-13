13900b87af9deda0df6299762e1e7aca4ee463a7
Vaibhavi <vaibhavi.s147@gmail.com>
2026-05-14 01:34:44 +0530

    Add Exciting Bets [A] from Codeforces

---
+++ 1543/A - Exciting Bets/solution.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define ios_fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+ 
+// Data Types
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+ 
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+ 
+// Macros
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define REP(i, n) for (int i = 0; i < n; ++i)
+ 
+// Modular Arithmetic
+ll mod_add(ll a, ll b) { return (a + b) % MOD; }
+ll mod_sub(ll a, ll b) { return (a - b + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a * b) % MOD; }
+ 
+int main() {
+    ios_fast;
+    int t;
+    cin >> t;
+    while (t--) {
+        // Problem logic here
+        ll a,b;
+        cin>>a>>b;
+        ll val=abs(a-b);
+        ll ans=0,ops=0;
+        if(val==0){
+            cout<<ans<<" "<<ops<<endl;
+        }else{
+            ops=min(val-a%val,a%val);
+            cout<<val<<" "<<ops<<endl;
+        }
+        
+ 
+                
+            
+        
+    }
+    return 0;
+}
