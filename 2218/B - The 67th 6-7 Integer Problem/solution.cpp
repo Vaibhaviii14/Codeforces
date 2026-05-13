416c7c2cf8cc29a3476b120f702e3ac03172c827
Vaibhavi <vaibhavi.s147@gmail.com>
2026-05-13 15:35:41 +0530

    Add The 67th 6-7 Integer Problem [B] from Codeforces

---
+++ 2218/B - The 67th 6-7 Integer Problem/solution.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define ios_fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+ 
+// Data Types
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+ 
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+ 
+// Macros
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define REP(i, n) for (int i = 0; i < n; ++i)
+ 
+// Modular Arithmetic
+ll mod_add(ll a, ll b) { return (a + b) % MOD; }
+ll mod_sub(ll a, ll b) { return (a - b + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a * b) % MOD; }
+ 
+int main() {
+    ios_fast;
+    int t;
+    cin >> t;
+    while (t--) {
+        // Problem logic here
+        vi v(7);
+        int sum=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<7;i++){
+            cin>>v[i];
+            sum+=v[i];
+            maxi=max(maxi,v[i]);
+        }
+        cout<<2*maxi-sum<<endl;
+ 
+ 
+                
+            
+        
+    }
+    return 0;
+}
