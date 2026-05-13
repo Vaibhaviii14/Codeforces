3c5f651bcbea6f2cd691c44fea0f7dae1614a355
Vaibhavi <vaibhavi.s147@gmail.com>
2026-05-13 15:32:22 +0530

    Add Line Trip [A] from Codeforces

---
+++ 1901/A - Line Trip/solution.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define ios_fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+ 
+// Data Types
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+ 
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+ 
+// Macros
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define REP(i, n) for (int i = 0; i < n; ++i)
+ 
+// Modular Arithmetic
+ll mod_add(ll a, ll b) { return (a + b) % MOD; }
+ll mod_sub(ll a, ll b) { return (a - b + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a * b) % MOD; }
+ 
+int main() {
+    ios_fast;
+    int t;
+    cin >> t;
+    while (t--) {
+        // Problem logic here
+        int n,x;
+        cin>>n>>x;
+        vector<int> nums(n);
+        for(int i=0;i<n;i++){
+            cin>>nums[i];
+        }
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            if(i==0){
+                maxi=max(maxi,nums[i]);
+            }else{
+                maxi=max(maxi,nums[i]-nums[i-1]);
+            }
+        }
+ 
+        maxi=max(maxi,2*(x-nums[n-1]));
+ 
+                
+        cout<<maxi<<endl;    
+        
+    }
+    return 0;
+}
