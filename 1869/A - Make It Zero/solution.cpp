39d436d3194214d2a5624c3fbec95833ae59a8e5
Vaibhavi <vaibhavi.s147@gmail.com>
2026-05-14 14:46:42 +0530

    Add Make It Zero [A] from Codeforces

---
+++ 1869/A - Make It Zero/solution.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define ios_fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+ 
+// Data Types
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+ 
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+ 
+// Macros
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define REP(i, n) for (int i = 0; i < n; ++i)
+ 
+// Modular Arithmetic
+ll mod_add(ll a, ll b) { return (a + b) % MOD; }
+ll mod_sub(ll a, ll b) { return (a - b + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a * b) % MOD; }
+ 
+int main() {
+    ios_fast;
+    int t;
+    cin >> t;
+    while (t--) {
+        // Problem logic here
+        int n;
+        cin>>n;
+        vll v(n);
+        for(int i=0;i<n;i++) cin>>v[i];
+        if(n%2==0){
+            cout<<2<<endl;
+            cout<<1<<" "<<n<<endl;
+            cout<<1<<" "<<n<<endl;
+        }else{
+            cout<<4<<endl;
+            cout<<1<<" "<<n-1<<endl;
+            cout<<1<<" "<<n-1<<endl;
+            cout<<n-1<<" "<<n<<endl;
+            cout<<n-1<<" "<<n<<endl;
+        }
+ 
+                
+            
+        
+    }
+    return 0;
+}
