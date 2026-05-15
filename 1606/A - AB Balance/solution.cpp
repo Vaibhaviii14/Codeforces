864a78f98e1c1d0a618061f9c753372f96f4230f
Vaibhavi <vaibhavi.s147@gmail.com>
2026-05-15 22:43:03 +0530

    Add AB Balance [A] from Codeforces

---
+++ 1606/A - AB Balance/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define ios_fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+ 
+// Data Types
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+ 
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+ 
+// Macros
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define REP(i, n) for (int i = 0; i < n; ++i)
+ 
+// Modular Arithmetic
+ll mod_add(ll a, ll b) { return (a + b) % MOD; }
+ll mod_sub(ll a, ll b) { return (a - b + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a * b) % MOD; }
+ 
+int main() {
+    ios_fast;
+    int t;
+    cin >> t;
+    while (t--) {
+        // Problem logic here
+        string s;
+        cin>>s;
+        int n=s.size();
+        if(s[0]!=s[n-1]){
+            s[0]=s[n-1];
+        }
+ 
+        cout<<s<<endl;
+ 
+                
+            
+        
+    }
+    return 0;
+}
