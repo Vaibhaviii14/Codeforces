0c23600a47c716a4d441a60530f6b71a66b297cf
Vaibhavi <vaibhavi.s147@gmail.com>
2026-06-04 22:20:16 +0530

    Add Minimum LCM [M] from Codeforces

---
+++ 1765/M - Minimum LCM/solution.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define ios_fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+ 
+// Data Types
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+ 
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+ 
+// Macros
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define REP(i, n) for (int i = 0; i < n; ++i)
+ 
+// Modular Arithmetic
+ll mod_add(ll a, ll b) { return (a + b) % MOD; }
+ll mod_sub(ll a, ll b) { return (a - b + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a * b) % MOD; }
+ 
+int main() {
+    ios_fast;
+    int t;
+    cin >> t;
+    while (t--) {
+        // Problem logic here
+        ll n;
+        cin>>n;
+        if(n%2==0){
+            cout<<n/2<<" "<<n/2<<endl;
+        }else{
+            ll fact=n;
+            for(int i=3;i*i<=n;i+=2){
+                if(n%i==0){
+                    fact=i;
+                    break;
+                }
+            }
+ 
+            ll a=n/fact;
+            ll b=n-a;
+ 
+            cout<<a<<" "<<b<<endl;
+        }
+        
+ 
+                
+            
+        
+    }
+    return 0;
+}
