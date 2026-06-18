7ace47cb05195792d45380d86bf52ad20aa30193
Vaibhavi <vaibhavi.s147@gmail.com>
2026-06-18 23:53:34 +0530

    Add Paint the Array [C] from Codeforces

---
+++ 1618/C - Paint the Array/solution.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Fast I/O
+#define ios_fast ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+ 
+// Data Types
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+ 
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+ 
+// Macros
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define REP(i, n) for (int i = 0; i < n; ++i)
+ 
+// Modular Arithmetic
+ll mod_add(ll a, ll b) { return (a + b) % MOD; }
+ll mod_sub(ll a, ll b) { return (a - b + MOD) % MOD; }
+ll mod_mul(ll a, ll b) { return (a * b) % MOD; }
+ 
+int main() {
+    ios_fast;
+    int t;
+    cin >> t;
+    while (t--) {
+        // Problem logic here
+        int n;
+        cin>>n;
+        vll v(n);
+        for(int i=0;i<n;i++) cin>>v[i];
+        ll odd=0,even=0;
+        for(int i=0;i<n;i++){
+            if(i&1==1){
+                odd=__gcd(odd,v[i]);
+            }else{
+                even=__gcd(even,v[i]);
+            }
+        }
+        bool evenok=true;
+        for(int i=1;i<n;i+=2){
+            if(v[i]%even==0){
+                evenok=false;
+                break;
+            }
+        }
+ 
+        bool oddok=true;
+        for(int i=0;i<n;i+=2){
+            if(v[i]%odd==0){
+                oddok=false;
+                break;
+            }
+        }
+ 
+        if(evenok){
+            cout<<even<<endl;
+            continue;
+        } 
+        if(oddok){
+            cout<<odd<<endl;
+            continue;
+        }else{
+            cout<<0<<endl;
+        }
+        
+ 
+                
+            
+        
+    }
+    return 0;
+}
